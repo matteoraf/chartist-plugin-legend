d936c960c314fb439201e1e418dd751f91e31582
matteoraf <matteoraf@users.noreply.github.com>
2020-05-16 02:32:12 +0200

    Update chartist-plugin-legend.js

--- chartist-plugin-legend.js
+++ chartist-plugin-legend.js
@@ -2,16 +2,16 @@
   if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["chartist"], function (a0) {
-      return (root['Chartist.plugins.legend'] = factory(a0));
+    define(['chartist'], function (chartist) {
+      return (root['Chartist.plugins.legend'] = factory(chartist));
     });
   } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require("chartist"));
+    module.exports = factory(require('chartist'));
   } else {
-    root['Chartist.plugins.legend'] = factory(root["Chartist"]);
+    root['Chartist.plugins.legend'] = factory(root['Chartist']);
   }
 }(this, function (Chartist) {
     /**
